c2c725313601218641d987f3fc9bfbf9db5bdace
Ubuntu <azureuser@vmkubernetes.xdg3at0ozixezdmpngocudltub.bx.internal.cloudapp.net>
2024-08-20 16:45:00 +0000

    snyk-docker

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Print a message
+echo "Hello, this is a simple script running inside the container."
+
+# Simulate some processing
+echo "Processing..."
+sleep 2
+
+# Print completion message
+echo "Processing complete!"
